ac3223f583b799482b98b13263a77870e01ca221
Ahmad <akhmedmassalkhi@gmail.com>
2024-11-28 03:28:27 +0400

    Add: Comments

--- NeuralNetwork/BackPropagation.py
+++ NeuralNetwork/BackPropagation.py
@@ -15,18 +15,17 @@ class Softmax_CategoricalCrossEntropy:
         self.loss.forward(self.softmax.output, y_real)
 
     def backward(self):
-        # y_real is expected to be one hot encoded
+        # make sure y_real is one hot encoded
         if self.y_real.shape != self.softmax.output.shape:
             self.y_real = np.eye(self.softmax.output.shape[1])[self.y_real]
         self.dLoss_dInputs = (self.softmax.output - self.y_real) / len(self.y_real)
         
-##################################################################
 
 class Layer:
     def __init__(self, nbOfInputs:int, nbOfOutputs:int) -> None:
         self.nbOfInputs = nbOfInputs
         self.nbOfOutputs = nbOfOutputs
-        self.lr = 0.01
+        self.lr = 0.01 # constant learning rate
         
         # initialize
         self.biases = np.zeros(nbOfOutputs)
@@ -37,9 +36,12 @@ class Layer:
         self.inputs = X
     
     def backward(self, dLoss_dNeurons):
+        # compute gradients
         self.dLoss_dWeights = np.dot(self.inputs.T, dLoss_dNeurons)
         self.dLoss_dBiases = np.average(dLoss_dNeurons, axis=0) # condense to 1xm dimensions
         self.dLoss_dInputs = np.dot(dLoss_dNeurons, self.weights.T)
+        
+        # descent gradients
         self.weights -= self.lr * self.dLoss_dWeights
         self.biases -= self.lr * self.dLoss_dBiases
 
