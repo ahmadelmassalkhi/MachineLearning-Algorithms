5b21b371018276c4919092aedee184faccb39a33
Ahmad <akhmedmassalkhi@gmail.com>
2024-12-07 02:53:07 +0400

    Implement L1 regularizer class

--- NeuralNetwork/lib/Regularizer.py
+++ NeuralNetwork/lib/Regularizer.py
@@ -29,19 +29,19 @@ class Regularizer:
 
 
 
+class L1(Regularizer):
+    def __init__(self, _lambda1=5e-7, _lambda2=5e-7):
+        super().__init__(_lambda1, _lambda2)
+
+    def backward(self, layer: Layer):
+        layer.dLoss_dWeights += self._lambda["weights"] * np.sign(layer.weights)
+        layer.dLoss_dBiases += self._lambda["biases"] * np.sign(layer.biases)
+
+        
 class L2(Regularizer):
     def __init__(self, _lambda1=5e-7, _lambda2=5e-7):
         super().__init__(_lambda1, _lambda2)
 
-    def add_loss(self, layer:Layer):
-        self.loss["weights"] += self._lambda["weights"] * np.sum(layer.weights ** 2)
-        self.loss["biases"] += self._lambda["biases"] * np.sum(layer.biases ** 2)
-
-    def get_loss(self):
-        return self.loss["weights"] + self.loss["biases"]
-
     def backward(self, layer: Layer):
         layer.dLoss_dWeights += 2 * self._lambda["weights"] * layer.weights
         layer.dLoss_dBiases += 2 * self._lambda["biases"] * layer.biases
-
-    
