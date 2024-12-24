e3ea07d7a9a17bdf6a9df8265302ef220e6f5a05
ahmadmassalkhi <massalkhiahmad18@gmail.com>
2024-12-25 01:58:34 +0200

    Simplify namings

--- NeuralNetwork/lib/Loss.py
+++ NeuralNetwork/lib/Loss.py
@@ -2,29 +2,30 @@ import numpy as np
 
 
 class Loss:
-    def forward(self, input, y_real):
+    def forward(self, prediction, real):
         raise NotImplementedError
     def backward(self):
         raise NotImplementedError
 
     @staticmethod
     def create(name: str) -> "Loss":
-        if name.lower() == 'categoricalcrossentropy': return CategoricalCrossEntropy()
+        if name.lower() == 'categorical_crossentropy': return CategoricalCrossEntropy()
         raise ValueError(f"Loss function {name} not supported.")
 
+
 class CategoricalCrossEntropy(Loss):
-    def forward(self, input, y_real):
+    def forward(self, prediction, real):
         # ensure one hot encoding
-        if y_real.shape != input.shape:
-            y_real = np.eye(input.shape[1])[y_real]
+        if real.shape != prediction.shape:
+            real = np.eye(prediction.shape[1])[real]
         
         # cache for backward pass
-        self.y_real = y_real
-        self.input = np.clip(input, 1e-7, 1-1e-7)
+        self.real = real
+        self.prediction = np.clip(prediction, 1e-7, 1-1e-7)
 
         # compute & return negative log likelihood
-        return -np.sum(y_real * np.log(self.input), axis=1)
+        return -np.sum(real * np.log(self.prediction), axis=1)
 
     def backward(self):
-        # compute & return dLoss_dInput
-        return - self.y_real / self.input # prevents exploding outputs (due to unnormalized/large inputs)
+        # compute & return dLoss_dprediction
+        return - self.real / self.prediction # prevents exploding outputs (due to unnormalized/large inputs)
